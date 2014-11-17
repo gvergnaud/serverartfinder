5a4817d3c08cdc4849d06e1b1cf5be579f5f2f7c
gvergnaud <gabriel.vergnaud@hetic.net>
2014-11-17 22:51:20 +0100

    allow 2

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
 var server={
     init : function(){
 
-        this.io = require('socket.io');
-        
-        io.configure('development', function(){
-            io.set('origins', '*:*');
-        }).listen(3000);
-        
+        this.io = require('socket.io').set('origins', '*:*').listen(3000);
+
         this.io.on('connection',this.listen);
     },
 
