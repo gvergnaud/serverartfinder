1f2537d2e27379f94f3c9cee4349199d79c46bc0
gvergnaud <gabriel.vergnaud@hetic.net>
2014-11-17 22:46:44 +0100

    allow origin

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 var server={
     init : function(){
 
-        this.io = require('socket.io').listen(3000);
+        this.io = require('socket.io');
+        
+        io.configure('development', function(){
+            io.set('origins', '*:*');
+        }).listen(3000);
+        
         this.io.on('connection',this.listen);
     },
 
